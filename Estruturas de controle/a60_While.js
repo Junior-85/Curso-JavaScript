c11ca3021183dbbe006b7c2ed1a2adb7958a4d6f
Junior-85 <gilmarvaz85@gmail.com>
2021-01-20 16:36:18 -0300

    While

--- Estruturas de controle/a60_While.js
+++ Estruturas de controle/a60_While.js
@@ -1,12 +1,12 @@
 function getInteiroAleatorioEntre(min, max) {
-    const valor = Math.ramdom() * (max - min) + min
+    const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
 
-let opcao = -1
+let opcao = 0
 
 while (opcao != -1) {
-    opcao = getInteiroAleatorioEntre(-1, 10)
+    opcao = getInteiroAleatorioEntre(-1, 5)
     console.log(`A opção escolhida foi ${opcao}.`)
 }
 
