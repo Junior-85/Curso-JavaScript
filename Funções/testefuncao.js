1d3a1f5ac33e992f1a8c0204d3ab90328358d60b
Junior-85 <gilmarvaz85@gmail.com>
2021-02-05 16:17:24 -0300

    Teste
    Testes nas chamdas das funções, operações com os retornos das funções.

---
+++ Funções/testefuncao.js
@@ -0,0 +1,44 @@
+let teste1 = function quadrado(valor) {
+    return valor * 2
+}
+
+//console.log(teste1(2))
+//let resTeste1 = teste1(2)
+//console.log(resTeste1)
+
+
+let teste2 = function quadrado2(valor) {
+    return valor * 2
+    //return console.log(valor * 3)
+}
+
+//teste2(3)
+//let resTeste2 = teste2(3)
+
+
+function testeDeFunção(a, b) {
+    let tes1 = teste1(a)
+    let tes2 = teste2(b)
+    return console.log(tes1 + tes2)
+}
+
+testeDeFunção(4, 5)
+
+console.log('---------------')
+
+
+let teste3 = function quadrado(valor) {
+    return valor * 2
+}
+
+console.log(teste3(2))
+//let resTeste1 = teste1(2)
+//console.log(resTeste1)
+
+
+let teste4 = function quadrado2(valor) {
+    return console.log(valor * 2)
+}
+
+teste4(3)
+//let resTeste2 = teste2(3)
