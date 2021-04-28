ec4422c93aaa30727ebbb8f191ddc88a480feb2f
Junior-85 <gilmarvaz85@gmail.com>
2021-04-28 20:04:52 -0300

    Valor do objeto
    Incluído teste para chamada de um valor do objeto

--- Funções/a84_funçãoFactroy02.js
+++ Funções/a84_funçãoFactroy02.js
@@ -10,5 +10,9 @@ function criarProduto(nome, preco ) {
 
 console.log(criarProduto('Abajur', 'R$ 123'))
 console.log(criarProduto('Notebook', 2990))
+var teste = criarProduto('PS4', 2.500)
+
+//Teste para exibir um valor do objeto
+console.log(teste.nome)
 
 /*Dentro do retorno da factory tambem podem ser colocadas funções*/
